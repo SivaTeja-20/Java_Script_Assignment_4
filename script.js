803bf94a99f9a5f6f068e5726184d27090d24208
SivaTeja-20 <99390827+SivaTeja-20@users.noreply.github.com>
2022-03-19 23:17:16 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,61 @@
+//Day calculation from date using "Zeller’s" Rule
+       
+function dayy(d,m,y){
+    var month = [0,11,12,1,2,3,4,5,6,7,8,9,10];
+    //m is the month number.
+      if (month[m]==11 || month[m]==12){
+        var  yy=y-1
+      }else{
+          yy=y
+      }
+    var y1 = yy%10;
+    var y2 = (yy-y1)%100;
+
+    //A is the last two digits of the year.
+    var A = y1+y2;
+
+    //B is the first two digits of the year.            
+    var B = (yy-A)/100;
+
+    //d is  the day of the month.
+    var dat = Math.floor( d+ ( (  (13*month[m])-1  )/5  ) + A  +   Math.floor(A/4)  +   Math.floor(B/4)  -  2*B  )
+    var data = dat%7
+   
+    if (data>0){
+          console.log(switch1(data))
+    }else{
+       console.log(switch1(data+7))
+    }
+}
+
+
+function switch1(data){
+
+    var day = ""
+    switch (data) {
+        case 0:
+            day = "sunday";
+            break;
+        case 1:
+            day = "monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:  
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+      }
+      return day
+}
+
+console.log(dayy(9,2,2022))
